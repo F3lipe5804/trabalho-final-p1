cc486857adc12787c1754e88f988c8a64c493372
F3lipe5804 <ycxp2wwgzs@privaterelay.appleid.com>
2025-11-14 16:50:38 -0300

    Update administrador.py

--- administrador.py
+++ administrador.py
@@ -9,12 +9,38 @@ class Administrador(Pessoa):
         print("\n=== Cadastro de Novo Usuário ===")
         nome = input("Novo cadastro: ")
         senha = input("Senha: ")
-        sql = "INSERT INTO usuarios (nome, senha) VALUES (%s, %s)"
-        cursor.execute(sql, (nome, senha))
+        sql = "INSERT INTO usuarios (nome, senha, tipo) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (nome, senha, "user"))
         conexao.commit()
         print(f"O {nome} foi cadastrado como Usuario!")
         cursor.close()
-        
+
+    def alterar_nivel_usuario(self, conexao):
+        cursor = conexao.cursor()
+        print("\n=== Alterar nível de permissão de usuário ===")
+        nome = input("Nome do usuário: ")
+        sql_select = "SELECT id, nome, tipo FROM usuarios WHERE nome = %s"
+        cursor.execute(sql_select, (nome,))
+        resultado = cursor.fetchone()
+
+        if not resultado:
+            print("Usuário não encontrado.")
+            cursor.close()
+            return
+
+        id_usuario, nome_usuario, tipo_atual = resultado
+
+        if tipo_atual == "adm":
+            novo_tipo = "user"
+        else:
+            novo_tipo = "adm"
+
+        sql_update = "UPDATE usuarios SET tipo = %s WHERE id = %s"
+        cursor.execute(sql_update, (novo_tipo, id_usuario))
+        conexao.commit()
+
+        print(f"Nível do usuário {nome_usuario} alterado de '{tipo_atual}' para '{novo_tipo}'.")
+        cursor.close()
 
     def agendar_consulta(self, paciente, data, conexao):
         cursor = conexao.cursor()
@@ -23,7 +49,6 @@ class Administrador(Pessoa):
         conexao.commit()
         print(f"Consulta para {paciente} agendada em {data}.")
         cursor.close()
-
 
     def reagendar_consulta(self, paciente, data_antiga, nova_data, conexao):
         cursor = conexao.cursor()
@@ -54,8 +79,8 @@ class Administrador(Pessoa):
 
         print(f"\n=== Agenda Médico ===")
         if resultados:
-            for paciente, data in resultados:
-                print(f"Paciente: {paciente} | Data: {data}")
+            for paciente, data, status in resultados:
+                print(f"Paciente: {paciente} | Data: {data} | Status: {status}")
         else:
             print("Nenhuma consulta registrada.")
         cursor.close()
